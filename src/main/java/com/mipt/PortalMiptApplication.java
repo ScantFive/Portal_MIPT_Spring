17a514474593354393f557f9a8e051363fec8ef8
Timofey <tvorobev158@gmail.com>
2026-03-11 22:28:33 +0300

    создан проект

---
+++ src/main/java/com/mipt/PortalMiptApplication.java
@@ -0,0 +1,13 @@
+package com.mipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalMiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalMiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mipt/PortalMiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalMiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
